185d13d4122de113761db55b5a3574b10672408c
Miguel Arriaga Velasco <marriagav3@gmail.com>
2024-05-29 00:01:40 -0600

    not working

--- mmseg/datasets/__init__.py
+++ mmseg/datasets/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 # yapf: disable
+#HERE
 from .ade import ADE20KDataset
 from .basesegdataset import BaseCDDataset, BaseSegDataset
 from .bdd100k import BDD100KDataset
--- mmseg/utils/class_names.py
+++ mmseg/utils/class_names.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+#HERE
 from mmengine.utils import is_str
 
 
