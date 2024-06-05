db1645391b2878faeb2054c9e96076c2b66215ca
Miguel Arriaga Velasco <marriagav3@gmail.com>
2024-06-04 18:14:30 -0600

    isanet and other

--- configs/gcnet/gcnet_r50-d8_4xb2-40k_deeplab-256x256.py
+++ configs/gcnet/gcnet_r50-d8_4xb2-40k_deeplab-256x256.py
@@ -1,7 +1,7 @@
 # HERE
 _base_ = [
     '../_base_/models/gcnet_r50-d8.py', '../_base_/datasets/deep_globe_dataset.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
 ]
 crop_size = (256, 256)
 data_preprocessor = dict(size=crop_size)
